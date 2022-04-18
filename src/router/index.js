eb913df00926f29f49c80682f10065d070f3ba8b
David Zhu <348867925@qq.com>
2022-04-18 10:45:24 +0800

    项目初始化-调整目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
+// 去掉原始路由表数组和删掉原始路由组件路径，view下的原始组件home和about，components下的helloworld和assets的静态资源图片
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
 ]
 
 const router = new VueRouter({
